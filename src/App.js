e1a86f25f82d7356475285ed7c0ecd1a039d80a8
Carlos Andres Gomez Daza <andresgd7@gmail.com>
2024-05-02 15:24:35 -0500

    ci: add Azure Static Web Apps workflow file on-behalf-of: @Azure opensource@microsoft.com

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './views/home/home.js'
+import Login from './views/login/login.js'
+import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
